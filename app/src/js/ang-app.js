bb1cc74b69608961c1ca7c4aed805c09890861fc
Rana Das <rana.pratap.das@gmail.com>
2016-11-13 08:28:17 +0000

    Adding two more ui states & partials

--- app/src/js/ang-app.js
+++ app/src/js/ang-app.js
@@ -8,14 +8,27 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
 
     $stateProvider
-    // HOME STATES AND NESTED VIEWS ========================================
+        // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
             templateUrl: 'partial-home.html'
         })
-
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
             // we'll get to this in a bit
-        });
+            url: '/about',
+            template: '<b> Rana Das</b> '
+
+        })
+
+        .state('courses' ,{
+            url: '/courses',
+            template: '<b> WIP</b> '
+        })
+
+        .state('contacts' ,{
+            url: '/contacts',
+            template: '<b> WIP!</b> '
+        })
+        ;
 });
