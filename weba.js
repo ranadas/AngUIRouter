49a38a802de52c6504bac19235a1e0c4f318e5b6
Rana Das <rana.pratap.das@gmail.com>
2016-11-14 08:31:26 +0000

    Allow CORS

--- weba.js
+++ weba.js
@@ -73,5 +73,22 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 
+// Allowing X-domain request
+var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+app.use(allowCrossDomain);
+
 app.listen(port);
-console.log("App listening on port " + port);
+console.log("NodeJs listening on port " + port);
