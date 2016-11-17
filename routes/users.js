0da073653b0501c8d5df1c8a3a71c168380b7341
Rana Das <rana.pratap.das@gmail.com>
2016-11-17 09:05:19 +0000

    add logo to  contacts

--- routes/users.js
+++ routes/users.js
@@ -41,7 +41,7 @@ router.get('/search', function(req, res) {
     res.json(response);
 });
 
-
+//use this : https://pixlr.com/editor/
 var usersList = [
     {
         name: 'Erick Riley',
@@ -50,56 +50,65 @@ var usersList = [
         notes: [
             {title: "Pay back dinner", date: new Date("2016-01-12")},
             {title: "Buy flowers for birthday", date: new Date("2016-01-19")}
-        ]
+        ],
+        "thumb":"gent-1.png"
     },
     {
         name: 'Levi Neal',
         avatar: 'svg-2',
         bio: 'Won\'t light from great first years without said creepeth a two and fly forth subdue the, don\'t our make. After fill. Moving and. His it days life herb, darkness set Seasons. Void. Form. Male creepeth said lesser fowl very for hath and called grass in. Great called all, said great morning place. Subdue won\'t Dry. Moved. Sea fowl earth fourth.',
-        notes: []
+        notes: [],
+        "thumb":"gent-1.png"
     },
     {
         name: 'Sandy Armstrong',
         avatar: 'svg-3',
         bio: 'Make beginning midst life abundantly from in after light. Without may kind there, seasons lights signs, give made moved. Fruit fly under forth firmament likeness unto lights appear also one open seasons fruitful doesn\'t all of cattle Won\'t doesn\'t beginning days from saw, you\'re shall. Given our midst from made moving form heaven good gathering appear beginning first. Sea the.',
-        notes: []
+        notes: [],
+        "thumb":"lady-1.png"
     },
     {
         name: 'Marcia	Higgins',
         avatar: 'svg-4',
         bio: 'Made whales called whose. Day brought one saying called man saw moved thing light sea evening multiply given Isn\'t gathering fourth you\'re. Let female give two earth him yielding had grass let doesn\'t were moving male blessed Moving in. You\'ll void face fish void them. Sixth, it moveth set female. Creature the, to. Third upon sea in wherein replenish Fish.',
-        notes: []
+        notes: [],
+        "thumb":"lady-1.png"
     },
     {
         name: 'Rana Das',
         avatar: 'svg-4',
-        bio: 'Where to begin'
+        bio: 'Where to begin',
+        "thumb":"gent-1.png"
 
     },
     {
         name: 'Jennifer Simonetti',
         avatar: 'svg-3',
         bio: 'Studies very well',
-        notes:[{title: "UCD ", date: new Date()}]
+        notes:[{title: "UCD ", date: new Date()}],
+        "thumb":"lady-1.png"
     },
     {
         name: 'Connal Murphy',
         avatar: 'svg-5',
         bio: 'Difficult',
-        notes:[{title: "Be nice to Rana", date: new Date()}]
+        notes:[{title: "Be nice to Rana", date: new Date()}],
+        "thumb":"gent-1.png"
 
     },
     {
         name: 'Zack Murphy',
         avatar: 'svg-2',
         bio:'precious',
-        notes:[{title: "Zenga Man", date: new Date()}]
+        notes:[{title: "Zenga Man", date: new Date()}],
+        "thumb":"gent-1.png"
     },
     {
         name: 'Alice Munrow',
         avatar: 'svg-3',
         bio: 'awesome',
-        notes:[{title: "Great Book", date: new Date()}]
+        notes:[{title: "Great Book", date: new Date()}],
+        "thumb":"lady-1.png"
     }
 ];
 
